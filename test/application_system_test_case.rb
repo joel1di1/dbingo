761e54c8abb851b0d7d422a05d6f35ae8c722d1f
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2022-01-20 00:22:34 +0100

    headless by default

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,8 +3,10 @@
 require 'test_helper'
 require 'rack_session_access/capybara'
 
+TEST_BROWSER = 'false' == ENV['HEADLESS'] ? :chrome : :headless_chrome
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: TEST_BROWSER, screen_size: [1400, 1400]
   include FactoryBot::Syntax::Methods
 
   def sign_in(user)
